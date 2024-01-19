78b55f8126f0b136e1ddc7e59e85ae83a643ca61
Crnastrela94 <luka940114@gmail.com>
2024-01-19 21:04:34 +0100

    update all done just style left

--- src/components/set-timer/setTimer.ts
+++ src/components/set-timer/setTimer.ts
@@ -1,6 +1,10 @@
 import Timer from "easytimer.js";
 
 export function setupTimer() {
+  if (document.getElementById("chronoExample")) {
+    // If it does, return early to prevent the rest of the code from executing
+    return;
+  }
   let timer = new Timer();
 
   // Create the HTML elements
@@ -26,49 +30,95 @@ export function setupTimer() {
 
   chronoExample.appendChild(valuesContainer);
 
-  let buttons = ["startButton", "stopButton"];
+  let startStopButton = document.createElement("button");
+  startStopButton.className = "startStopButton";
+  startStopButton.textContent = "Start Timer";
+  chronoExample.appendChild(startStopButton);
 
-  buttons.forEach((button) => {
-    let btn = document.createElement("button");
-    btn.className = button;
-    btn.textContent = button.charAt(0).toUpperCase() + button.slice(1);
-    chronoExample.appendChild(btn);
-  });
+  let checkBoxWrapper = document.createElement("div");
+  checkBoxWrapper.className = "checkBoxWrapper";
+  chronoExample.appendChild(checkBoxWrapper);
+
+  let checkBoxInterval = document.createElement("input");
+  checkBoxInterval.type = "checkbox";
+  checkBoxInterval.id = "checkBoxInterval";
+  let intervalLabel = document.createElement("label");
+  intervalLabel.htmlFor = "checkBoxInterval";
+  intervalLabel.textContent = "Interval";
+  checkBoxWrapper.appendChild(checkBoxInterval);
+  checkBoxWrapper.appendChild(intervalLabel);
+
+  let checkBoxWrapperSleep = document.createElement("div");
+  checkBoxWrapperSleep.className = "checkBoxWrapper";
+  chronoExample.appendChild(checkBoxWrapperSleep);
+
+  let checkBoxSleep = document.createElement("input");
+  checkBoxSleep.type = "checkbox";
+  checkBoxSleep.id = "checkBoxSleep";
+  let sleepLabel = document.createElement("label");
+  sleepLabel.htmlFor = "checkBoxSleep";
+  sleepLabel.textContent = "Sleep 5 min";
+  checkBoxWrapperSleep.appendChild(checkBoxSleep);
+  checkBoxWrapperSleep.appendChild(sleepLabel);
 
   // Append the chronoExample element to the .allInfo element
   let allInfo = document.querySelector(".allInfo")!;
   allInfo.appendChild(chronoExample);
 
   // Initialize the countdown time
-  let countdownTime = 30;
+  let countdownTime = {
+    seconds: 30,
+    setTime: 30,
+  };
 
   // Add event listeners to the buttons
   document
-    .querySelector("#chronoExample .startButton")!
+    .querySelector("#chronoExample .startStopButton")!
     .addEventListener("click", function () {
-      timer.start({ countdown: true, startValues: { seconds: countdownTime } });
-    });
-
-  document
-    .querySelector("#chronoExample .stopButton")!
-    .addEventListener("click", function () {
-      timer.stop();
+      if (timer.isRunning() == false) {
+        startStopButton.textContent = "Stop Timer";
+        timer.start({
+          countdown: true,
+          startValues: { seconds: countdownTime.seconds },
+        });
+      } else {
+        startStopButton.textContent = "Start Timer";
+        countdownTime.seconds = timer.getTimeValues().seconds;
+        timer.stop();
+      }
     });
 
   document
     .querySelector("#chronoExample .increaseButton")!
     .addEventListener("click", function () {
-      countdownTime += 10;
+      countdownTime.setTime += 10;
+      countdownTime.seconds = countdownTime.setTime;
       document.querySelector("#chronoExample .values")!.textContent =
-        "00:00:" + countdownTime;
+        "00:00:" + countdownTime.seconds;
     });
 
   document
     .querySelector("#chronoExample .decreaseButton")!
     .addEventListener("click", function () {
-      countdownTime = Math.max(0, countdownTime - 10);
+      countdownTime.setTime = Math.max(0, countdownTime.setTime - 10);
+      countdownTime.seconds = countdownTime.setTime;
       document.querySelector("#chronoExample .values")!.textContent =
-        "00:00:" + countdownTime;
+        "00:00:" + countdownTime.seconds;
+    });
+  document
+    .querySelector("#chronoExample #checkBoxInterval")!
+    .addEventListener("click", function () {
+      if (checkBoxInterval.checked) {
+        checkBoxInterval.checked = true;
+      }
+    });
+
+  document
+    .querySelector("#chronoExample #checkBoxSleep")!
+    .addEventListener("click", function () {
+      if (checkBoxSleep.checked) {
+        checkBoxSleep.checked = true;
+      }
     });
 
   // Update the timer display
@@ -83,6 +133,27 @@ export function setupTimer() {
     let iframe = document.createElement("iframe");
     iframe.src = "https://www.youtube.com/embed/rUkzZTGE6jI?autoplay=1";
     iframe.allow = "autoplay";
+    countdownTime.seconds = countdownTime.setTime;
+
+    if (checkBoxInterval.checked == true || checkBoxSleep.checked == true) {
+      if (checkBoxSleep.checked == true) {
+        setTimeout(function () {
+          timer.start({
+            countdown: true,
+            startValues: { seconds: countdownTime.seconds },
+          });
+        }, 300000);
+      } else {
+        timer.start({
+          countdown: true,
+          startValues: { seconds: countdownTime.seconds },
+        });
+      }
+    }
+    if (timer.isRunning() == false) {
+      startStopButton.textContent = "Reset Timer";
+    }
+
     allInfo.appendChild(iframe);
   });
 }
--- src/components/timerDigital/timerdigital.ts
+++ src/components/timerDigital/timerdigital.ts
@@ -5,25 +5,29 @@ export function timerDigital(): void {
 
   let timer: Timer = new Timer();
 
+  // Create elements once
+  const allInfo: HTMLDivElement =
+    document.querySelector<HTMLDivElement>(".allInfo")!;
+  allInfo.innerHTML = `<header id="timer-digital-header"><div>ham</div><h1>interval</h1></header><div id="timer-digital-numbers"></div>`;
+  const timerNumbers: HTMLDivElement = document.querySelector<HTMLDivElement>(
+    "#timer-digital-numbers"
+  )!;
+  const abortButton: HTMLButtonElement = document.createElement("button");
+  abortButton.textContent = "ABORT TIMER";
+  abortButton.setAttribute("id", "abort-button");
+  allInfo.appendChild(abortButton);
+
   timer.addEventListener("secondsUpdated", function () {
     let secondCounter: string = timer.getTimeValues().seconds.toString();
     let minuteCounter: string = timer.getTimeValues().minutes.toString();
 
-    const allInfo: HTMLDivElement =
-      document.querySelector<HTMLDivElement>(".allInfo")!;
-    allInfo.innerHTML = `<header id="timer-digital-header"><div>ham</div><h1>interval</h1></header>`;
-    allInfo.innerHTML += `<div id="timer-digital-numbers">${minuteCounter} : ${secondCounter}</div>`;
+    // Update text content of timer values
+    timerNumbers.textContent = `${minuteCounter} : ${secondCounter}`;
+  });
 
-    // Abort button
-    const abortButton: HTMLButtonElement = document.createElement("button");
-    abortButton.textContent = "ABORT TIMER";
-    abortButton.setAttribute("id", "abort-button");
-    allInfo.appendChild(abortButton);
-
-    abortButton.addEventListener("click", () => {
-      console.log("aborted");
-      timer.reset();
-    });
+  abortButton.addEventListener("click", () => {
+    console.log("aborted");
+    timer.reset();
   });
 
   timer.start();
