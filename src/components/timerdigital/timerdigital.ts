3d0e8bffb3c908da1b92f3d1d39827136aaee3c5
Lars Abelsson <georg483@msn.com>
2024-01-22 14:03:14 +0100

    removed navicon and some sass. missed to add all.

--- src/components/timerdigital/timerdigital.ts
+++ src/components/timerdigital/timerdigital.ts
@@ -1,46 +1,39 @@
-import Timer from "easytimer.js";
+import Timer from "easytimer.js"
 
 export function timerDigital() {
-  let timer: Timer = new Timer();
+  let timer: Timer = new Timer()
+
   const app: HTMLDivElement =
-    document.querySelector<HTMLDivElement>(".allInfo")!;
-  app.innerHTML = `<section id="timer-digital-section"></section>`;
+    document.querySelector<HTMLDivElement>(".allInfo")!
+  app.innerHTML = `<section id="timer-digital-section"></section>`
   const section: HTMLDivElement | null = document.querySelector<HTMLDivElement>(
     "#timer-digital-section"
-  );
+  )
 
   timer.addEventListener("secondsUpdated", function () {
-    let secondCounter: string = timer.getTimeValues().seconds.toString();
-    let minuteCounter: string = timer.getTimeValues().minutes.toString();
+    let secondCounter: string = timer.getTimeValues().seconds.toString()
+    let minuteCounter: string = timer.getTimeValues().minutes.toString()
 
     if (section) {
-      section.innerHTML = `<header id="timer-digital-header"><div id="timer-digital-navicon"><img  src="/navicon.svg" alt="navicon" /></div><div><h1>interval</h1></div></header>`;
-      section.innerHTML += `<div id="timer-digital-numbers">${minuteCounter} : ${secondCounter}</div>`;
+      section.innerHTML = `<header id="timer-digital-header"><h1>interval</h1></header>`
+      section.innerHTML += `<div id="timer-digital-numbers">${minuteCounter} : ${secondCounter}</div>`
 
-      // menu listener
-      const menu: HTMLDivElement | null =
-        document.querySelector<HTMLDivElement>("#timer-digital-navicon");
-      if (menu) {
-        menu.addEventListener("click", () => {
-          timer.stop();
-        });
-      }
       // Abort button
-      const abortButton: HTMLButtonElement = document.createElement("button");
-      abortButton.textContent = "ABORT TIMER";
-      abortButton.setAttribute("class", "abort-button");
-      section.appendChild(abortButton);
+      const resetButton: HTMLButtonElement = document.createElement("button")
+      resetButton.textContent = "RESET TIMER"
+      resetButton.setAttribute("class", "abort-button")
+      section.appendChild(resetButton)
 
-      abortButton.addEventListener("click", () => {
-        timer.reset();
-      });
+      resetButton.addEventListener("click", () => {
+        timer.reset()
+      })
     }
-  });
-  timer.start();
+  })
+  timer.start()
 
   return function stop() {
-    timer.stop();
+    timer.stop()
 
-    section!.remove();
-  };
+    section!.remove()
+  }
 }
