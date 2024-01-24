68e64a17078325d78f4f617e50b2c8fa0e66d7bc
pjkbsn <philip.jkbsn@gmail.com>
2024-01-24 15:44:00 +0100

    Fixed alarmview .ts after merge

--- src/components/alarmview/alarmview.ts
+++ src/components/alarmview/alarmview.ts
@@ -1,48 +1,47 @@
 import alarm from "./img/alarm.svg";
 
 export function timesUp() {
-  const app: HTMLDivElement | null = document.querySelector(".allInfo");
+    const app: HTMLDivElement | null = document.querySelector(".allInfo");
 
-  const alarmViewWrapper = document.createElement("div");
-  alarmViewWrapper.setAttribute("class", "alarm-view-wrapper");
-  app?.appendChild(alarmViewWrapper);
+    const alarmViewWrapper: HTMLDivElement = document.createElement("div");
+    alarmViewWrapper.setAttribute("class", "alarm-view-wrapper");
+    app?.appendChild(alarmViewWrapper);
 
-  const alarmViewInnerWrapper = document.createElement("div");
-  alarmViewInnerWrapper.setAttribute("class", "alarm-view-inner-wrapper");
-  alarmViewWrapper.appendChild(alarmViewInnerWrapper);
+    const alarmViewInnerWrapper: HTMLDivElement = document.createElement("div");
+    alarmViewInnerWrapper.setAttribute("class", "alarm-view-inner-wrapper");
+    alarmViewWrapper.appendChild(alarmViewInnerWrapper);
 
-  const alarmView = document.createElement("div");
-  alarmView.setAttribute("class", "alarm-view");
-  alarmViewInnerWrapper.appendChild(alarmView);
+    const alarmView: HTMLDivElement = document.createElement("div");
+    alarmView.setAttribute("class", "alarm-view");
+    alarmViewInnerWrapper.appendChild(alarmView);
 
-  const clockContainer: HTMLDivElement = document.createElement("div");
-  clockContainer.setAttribute("class", "clock-container");
-  alarmView.appendChild(clockContainer);
+    const clockContainer: HTMLDivElement = document.createElement("div");
+    clockContainer.setAttribute("class", "clock-container");
+    alarmView.appendChild(clockContainer);
 
-  const clockLogo: HTMLImageElement = document.createElement("img");
-  clockLogo.src = "../components/alarview/img/alarm.svg";
-  clockLogo.setAttribute("class", "clock-logo");
-  clockContainer.appendChild(clockLogo);
+    const clockLogo: HTMLImageElement = document.createElement("img");
+    clockLogo.setAttribute("class", "clock-logo");
+    clockLogo.src = alarm;
+    clockContainer.appendChild(clockLogo);
 
-  clockLogo.src = alarm;
-  clockContainer.appendChild(clockLogo);
+    const clockText: HTMLParagraphElement = document.createElement('p');
+    clockText.setAttribute('class', 'clock-text');
+    clockText.innerHTML = 'Times Up!';
+    alarmView.appendChild(clockText);
 
-  const newTimer: HTMLButtonElement = document.createElement("button");
-  newTimer.textContent = "SET NEW TIMER";
-  newTimer.setAttribute("class", "new-timer");
-  alarmView.appendChild(newTimer);
-  alarmView.appendChild(newTimer);
+    const newTimer: HTMLButtonElement = document.createElement("button");
+    newTimer.textContent = "SET NEW TIMER";
+    newTimer.setAttribute("class", "new-timer");
+    alarmView.appendChild(newTimer);
 
-  if (alarmViewWrapper) {
-    alarmViewWrapper.style.display = "block";
-    alarmViewWrapper.classList.add("full-screen");
+    if (alarmViewWrapper) {
+        alarmViewWrapper.style.display = "block";
+        alarmViewWrapper.classList.add("full-screen");
 
-    newTimer.addEventListener("click", () => {
-      console.log("knappen trycktes");
-      // återgå till setTimer här
-
-      alarmViewWrapper.style.display = "none";
-      alarmViewWrapper.remove();
-    });
-  }
+        newTimer.addEventListener("click", () => {
+            // Stänger overlay när knappen blir tryckt.
+            alarmViewWrapper.style.display = "none";
+            alarmViewWrapper.remove();
+        });
+    }
 }
